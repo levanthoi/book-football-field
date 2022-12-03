ba3482ecebad53921a7cfc69ba1a4bca2689e68b
Lê Văn Thời <lethoi1506@gmail.com>
2022-12-03 19:21:51 +0700

    project structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Router, { useRouter } from "next/router";
+import NProgress from "nprogress";
 
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log("pageProps", pageProps);
+  // console.log("Component", Component);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  ctx.roles = [];
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps({ ...ctx });
+  }
+  const initialNow = Date.now();
+  return { pageProps, initialNow };
+};
+
+export default MyApp;
